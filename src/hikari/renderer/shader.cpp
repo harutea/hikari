1be9494f7d84688ce0cc03e79269dcff2d91edca
harutea <snowhanami@gmail.com>
2023-10-03 19:41:26 +0900

    define class: Shader

---
+++ src/hikari/renderer/shader.cpp
@@ -0,0 +1,15 @@
+#include "shader.h"
+
+using namespace hikari;
+
+Shader::Shader(const char *vertexShaderPath, const char *fragmentShaderPath)
+{
+}
+
+Shader::~Shader()
+{
+}
+
+void Shader::use()
+{
+}
