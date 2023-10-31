747f36c3e633da66c7894f6549878a8fdedc5751
harutea <snowhanami@gmail.com>
2023-10-31 11:04:57 +0900

    finish defining Shader class

--- src/hikari/renderer/renderer.cpp
+++ src/hikari/renderer/renderer.cpp
@@ -1,7 +1,7 @@
 #include "renderer.h"
 #include <iostream>
 #include <cstring>
-#include "shader.h"
+#include <cmath>
 
 using namespace hikari;
 
@@ -15,21 +15,6 @@ Renderer::~Renderer()
 
 void Renderer::setup()
 {
-    vertexShaderSource = new char[1000];
-    strcpy(vertexShaderSource, "#version 330 core\n"
-                               "layout (location = 0) in vec3 aPos;\n"
-                               "void main()\n"
-                               "{\n"
-                               "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-                               "}\0");
-    fragmentShaderSource = new char[1000];
-    strcpy(fragmentShaderSource, "#version 330 core\n"
-                                 "out vec4 FragColor;\n"
-                                 "\n"
-                                 "void main()\n"
-                                 "{\n"
-                                 "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-                                 "}\0");
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -44,25 +29,10 @@ void Renderer::setup()
     }
 
     glfwMakeContextCurrent(window);
-    gladLoadGL((GLADloadfunc)glfwGetProcAddress);
+    gladLoadGL(glfwGetProcAddress);
     // glfwSwapInterval(1);
 
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-
-    shaderProgram = glCreateProgram();
-
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
+    this->triangleShader = new Shader("../shaders/triangle.vert", "../shaders/triangle.frag");
 
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
@@ -76,13 +46,14 @@ void Renderer::setup()
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
-    glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, &Renderer::framebuffer_size_callback);
 }
 
@@ -91,10 +62,16 @@ void Renderer::render()
     while (!glfwWindowShouldClose(window))
     {
         this->processInput(window);
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClearColor(0.9f, 0.9f, 0.9f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glUseProgram(shaderProgram);
+        triangleShader->use();
+
+        // float timeValue = glfwGetTime();
+        // float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+        // int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+        // glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
@@ -108,7 +85,6 @@ void Renderer::clear()
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     glDeleteBuffers(1, &EBO);
-    glDeleteProgram(shaderProgram);
 
     glfwTerminate();
 }
